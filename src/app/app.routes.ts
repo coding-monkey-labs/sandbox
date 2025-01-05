e7aedad3c62d575027b9d0a0c7c262bd1cb04a3e
maheshk172 <kulkarni.mak172@gmail.com>
2025-01-05 18:15:54 +0530

    Added modules, components, bootstrap and much more

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ServicesComponent } from './services/services.component';
+import { SolutionsComponent } from './solutions/solutions.component';
+import { ContactComponent } from './contact/contact.component';
+
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'services', component: ServicesComponent },
+  { path: 'solutions', component: SolutionsComponent },
+  { path: 'contact', component: ContactComponent },
+];
